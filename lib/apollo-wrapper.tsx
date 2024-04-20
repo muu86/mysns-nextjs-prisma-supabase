6bd2a690734465b4de6f1e8a88cbea3f177beed6
mj <mu8786@gmail.com>
2024-04-20 14:27:02 +0900

    apollo client url 변경-2

--- lib/apollo-wrapper.tsx
+++ lib/apollo-wrapper.tsx
@@ -49,7 +49,10 @@ export const sseLink = new SSELink({
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: process.env.VERCEL === '1' ? `${process.env.VERCEL_URL}/api/graphql` : 'http://localhost:3000/api/graphql',
+    uri:
+      process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+        ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+        : 'http://localhost:3000/api/graphql',
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: 'no-store' },
